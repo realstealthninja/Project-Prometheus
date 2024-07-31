2b3f91803b6865a6cad592b6e53e1254144ce067
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-07-31 12:14:31 +0530

    fix: remove old files

--- scripts/Sunflower.gd
+++
@@ -1,9 +0,0 @@
-extends "res://scripts/Monster.gd"
-
-func _ready():
-	super()
-	SPEED=0
-
-
-func _physics_process(delta):
-	pass
--- scripts/properties/player_properties.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-@export var health: int = 3
-@export var max_health: int = 3
-@export var actions: int  = 5
-@export var max_moves: int = 1
